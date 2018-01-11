4da8c80b4faf27dc44ec91f318c0cdcfe6f8a062
aro94 <arayik.94.grigoryan@gmail.com>
2018-01-11 17:13:23 +0400

    improve

--- src/Traits/CounterCache.php
+++ src/Traits/CounterCache.php
@@ -2,12 +2,22 @@
 
 namespace LaraCounterCache\Traits;
 
+use DB;
+
 /**
  * Trait CounterCache
  * @package CounterCache\Traits
  */
 trait CounterCache
 {
+    public $ignorCounterCache = false;
+
+    private $counterData = [];
+
+    private $queryCounter;
+
+    private $tableCounter;
+
     /**
      *
      */
@@ -15,21 +25,110 @@ trait CounterCache
     {
         parent::boot();
 
-        static::created(function ($model) {
-
-        });
-
         static::saved(function ($model) {
-
-        });
-
-        static::updated(function ($model){
-
+            if (method_exists($model, 'addCounter')) {
+                $model->addCounter();
+                $model->runCounter('up');
+            }
         });
 
         static::deleted(function ($model) {
-
+            if (method_exists($model, 'addCounter')) {
+                $model->addCounter();
+                $model->runCounter('down');
+            }
         });
+
+    }
+
+    public function runCounter($type)
+    {
+        $this->generateQueryCounter();
+        if ($type === 'up') {
+            $this->counterUp();
+        }
+        if ($type === 'down') {
+            $this->counterDown();
+        }
+    }
+
+    private function generateQueryCounter()
+    {
+        foreach ($this->counterData as $table => $datum) {
+            $this->queryCounter = $this->load([$table => function ($query) {
+                $query->select('id');
+            }])->$table;
+
+            if (!is_array($datum)) {
+                $datum = [$datum];
+            }
+
+            foreach ($datum as $key => $item) {
+                if (is_numeric($key)) {
+                    $this->queryCounter->increment($item);
+                } else {
+                    $this->checkByParams($key, $item);
+                }
+            }
+
+        }
+    }
+
+    private function checkByParams($name, $attr)
+    {
+        if (!empty($attr['conditions'])) {
+            if (is_array($attr['conditions'])) {
+                $this->addBaseCondition(...$attr['conditions']);
+            }
+            if ($attr['conditions'] instanceof \Closure) {
+                $query = $this->addCustomCondition($attr['conditions']);
+            }
+
+            unset($attr['conditions']);
+            $query->where('id',$this->queryCounter->id)->increment($name);
+        }
+    }
+
+    private function addBaseCondition($column, $value, $cmp = '=')
+    {
+        if (is_array($value)) {
+            $this->queryCounter->whereIn($column, $value);
+        } else {
+            $this->queryCounter->where($column, $cmp, $value);
+        }
+    }
+
+    /**
+     * @param callable $func
+     * @return mixed
+     */
+    private function addCustomCondition(callable $func)
+    {
+        $query = $this->queryCounter->newQueryWithoutScopes();
+        $func($query);
+        return $query->getQuery();
+    }
+
+    private function checkQueryColumns($data, $event = '+', $i = 1)
+    {
+        $updateColumns = [];
+        foreach ($data as $key => $item) {
+            if (is_numeric($key)) {
+                $updateColumns[$item] = DB::raw($item . ' ' . $event . ' ' . $i);
+            } else {
+                $updateColumns[$key] = DB::raw($key . ' ' . $event . ' ' . $item);
+            }
+        }
+        return $updateColumns;
+    }
+
+    private function counterUp()
+    {
+
+    }
+
+    private function counterDown()
+    {
 
     }
 
